e3b517d793c44c8b17b30f072e9feeec841c33ab
Cian Keegan <161904946+ckeegan12@users.noreply.github.com>
2025-09-04 02:41:17 +0000

    import libs

--- lib/CNN_Digit_Rec.py
+++ lib/CNN_Digit_Rec.py
@@ -1,1 +1,19 @@
+import pandas as pd
+import numpy as np 
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets
 
+train_dataset = torchvision.datasets.MNIST(
+        root='./data',        
+        train=True,           
+        download=True,        
+        transform=None  
+)
+
+mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
+mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
+
+mnist_testset.head()
